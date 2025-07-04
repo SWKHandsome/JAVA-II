7171dc90ac4037b370505670bdfff8761a06a805
SWKHandsome <b240150B@sc.edu.my>
2025-07-04 11:04:02 +0800

    Create FirstPage.java

---
+++ FirstPage.java
@@ -0,0 +1,23 @@
+import javafx.application.Application;
+import javafx.scene.Scene;  
+import javafx.scene.control.Button;  
+import javafx.stage.Stage;  
+import javafx.scene.layout.StackPane;  
+
+public class FirstPage extends Application{  
+  
+    @Override  
+    public void start(Stage primaryStage) throws Exception {
+	Button btn1=new Button("Say, Hello World"); 
+	StackPane root=new StackPane();  
+        root.getChildren().add(btn1);
+	    Scene scene=new Scene(root,600,400);
+	    primaryStage.setTitle("First JavaFX Application");  
+        primaryStage.setScene(scene);  
+    	primaryStage.show();       
+   }
+   public static void main (String[] args)  
+    {  
+        launch(args);  
+    } 
+}
